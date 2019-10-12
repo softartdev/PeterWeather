5f12b2a13292cda5861256daa19b0e0debc78dde
artur <artik222012@gmail.com>
2019-10-12 05:11:32 +0500

    create view model

--- app/src/main/java/com/softartdev/peterweather/MainActivity.kt
+++ app/src/main/java/com/softartdev/peterweather/MainActivity.kt
@@ -2,11 +2,15 @@ package com.softartdev.peterweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 }
